0986e287fec7b4529cdda67bf0ecc7210704b342
胡啸宇 <huxy@weipaitang.com>
2021-03-20 11:57:40 +0800

    创建数据表

--- main.go
+++ main.go
@@ -21,28 +21,35 @@ var router = mux.NewRouter()
 
 var db *sql.DB
 
-func initDB()  {
+func initDB() {
+
 	var err error
 	config := mysql.Config{
-		User: "root",
-		Passwd: "123456",
-		Addr: "127.0.0.1:3306",
-		Net: "tcp",
+		User:                 "root",
+		Passwd:               "123456",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblog",
 		AllowNativePasswords: true,
 	}
 
-	db, err := sql.Open("mysql", config.FormatDSN())
+	// 准备数据库连接池
+	db, err = sql.Open("mysql", config.FormatDSN())
 	checkError(err)
 
+	// 设置最大连接数
 	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
 	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
 	db.SetConnMaxLifetime(5 * time.Minute)
 
+	// 尝试连接，失败会报错
 	err = db.Ping()
 	checkError(err)
 }
 
-func checkError(err error)  {
+func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -162,7 +169,21 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+    id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    body longtext COLLATE utf8mb4_unicode_ci
+); `
+
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+}
+
 func main() {
+
+	initDB()
+	createTables()
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
